9729a8053eb7d06fb360c50faa227801519ab895
Jarrettsmith28 <79722509+Jarrettsmith28@users.noreply.github.com>
2022-09-27 16:08:17 -0600

    Add files via upload

---
+++ Tic Tac Toe/Tic Tac Toe/Game.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe
+{
+  internal class Game
+  {
+
+    public static char[] GameState { get; set; } = new char[9] { '0', '1', '2', '3', '4', '5', '6', '7', '8' };
+
+    public String GameDisplay { get; set; } = $"{GameState[0]}|{GameState[1]}|{GameState[2]}\n{GameState[3]}|{GameState[4]}|{GameState[5]}\n{GameState[6]}|{GameState[7]}|{GameState[8]}";
+
+
+    public string Mark(int player, int mark)
+    {
+      if (GameState[mark] == 'x' || GameState[mark] == 'o')
+      {
+        return "there is already a mark there";
+      }
+      if (player == 1)
+      {
+        GameState[mark] = 'x';
+      }
+      if (player == 2)
+      {
+        GameState[mark] = 'o';
+      }
+      
+
+      var game = new Game();
+      return game.GameDisplay;
+    }
+
+    public string? GameOver(string player1, string player2)
+    {
+      if (Game.GameState[0] == 'x' && Game.GameState[1] == 'x' && Game.GameState[2] == 'x') return $"{player1} wins";
+      if (Game.GameState[0] == 'x' && Game.GameState[3] == 'x' && Game.GameState[6] == 'x') return $"{player1} wins";
+      if (Game.GameState[0] == 'x' && Game.GameState[4] == 'x' && Game.GameState[8] == 'x') return $"{player1} wins";
+      if (Game.GameState[1] == 'x' && Game.GameState[4] == 'x' && Game.GameState[7] == 'x') return $"{player1} wins";
+      if (Game.GameState[2] == 'x' && Game.GameState[4] == 'x' && Game.GameState[6] == 'x') return $"{player1} wins";
+      if (Game.GameState[2] == 'x' && Game.GameState[5] == 'x' && Game.GameState[8] == 'x') return $"{player1} wins";
+      if (Game.GameState[3] == 'x' && Game.GameState[4] == 'x' && Game.GameState[5] == 'x') return $"{player1} wins";
+      if (Game.GameState[6] == 'x' && Game.GameState[7] == 'x' && Game.GameState[8] == 'x') return $"{player1} wins";
+      if (Game.GameState[0] == 'o' && Game.GameState[1] == 'o' && Game.GameState[2] == 'o') return $"{player2} wins";
+      if (Game.GameState[0] == 'o' && Game.GameState[3] == 'o' && Game.GameState[6] == 'o') return $"{player2} wins";
+      if (Game.GameState[0] == 'o' && Game.GameState[4] == 'o' && Game.GameState[8] == 'o') return $"{player2} wins";
+      if (Game.GameState[1] == 'o' && Game.GameState[4] == 'o' && Game.GameState[7] == 'o') return $"{player2} wins";
+      if (Game.GameState[2] == 'o' && Game.GameState[4] == 'o' && Game.GameState[6] == 'o') return $"{player2} wins";
+      if (Game.GameState[2] == 'o' && Game.GameState[5] == 'o' && Game.GameState[8] == 'o') return $"{player2} wins";
+      if (Game.GameState[3] == 'o' && Game.GameState[4] == 'o' && Game.GameState[5] == 'o') return $"{player2} wins";
+      if (Game.GameState[6] == 'o' && Game.GameState[7] == 'o' && Game.GameState[8] == 'o') return $"{player2} wins";
+      else return null;
+    }
+  }
+}
---
+++ Tic Tac Toe/Tic Tac Toe/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Transactions;
+using Tic_Tac_Toe;
+Console.WriteLine("Welcome to my Tic Tac Toe game please enter the name for player 1");
+var player1 = Console.ReadLine();
+Console.WriteLine("What is player 2's name?");
+var player2 = Console.ReadLine();
+var game = new Game();
+Console.WriteLine(game.GameDisplay);
+int count = 0;
+for (int i = 1; game.GameOver(player1, player2) == null; count++)
+{
+  Console.WriteLine($"player {i} your turn please choose a number to indicate where you would like to place your mark");
+  var mark = Convert.ToInt32(Console.ReadLine());
+  var response = game.Mark(i, mark);
+  Console.WriteLine(response);
+  if (response.Contains("already"))
+  {
+    Console.WriteLine($"player {i} your turn please choose a number to indicate where you would like to place your mark");
+    mark = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(game.Mark(i, mark));
+  }
+  if (count >= 8) break;
+  else if (i == 1) i++;
+  else if (i == 2) i--;
+}
+Console.WriteLine(game.GameOver(player1, player2));
+if (count >= 8 && game.GameOver(player1, player2) == null) Console.WriteLine("cats game. nobody wins");
